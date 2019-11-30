b30805703922048f5a3599a6033d7317e41cd536
Przemyslaw Cieslak <56787381+PrzemyslawCieslak@users.noreply.github.com>
2019-11-30 12:25:35 +0100

    Klasy decyzyjne + ilość elementów

--- C01/c01.py
+++ C01/c01.py
@@ -1,34 +1,114 @@
-plik = open("C01/diabetes.txt", "r")
-plik2 = open("C01/diabetes-type.txt", "r")
-
-slownik = {}
-slownik2 = {}
-
-for x in plik2:
-    line = x
-    line = line.strip()
-    line = line.split(" ")
-    slownik2[line[0]] = line[1]
-
-indexesOfNumbers = []
-i = 1
-for key in slownik2:
-    if slownik2[key] == "n":
-        indexesOfNumbers.append(i)
-    i += 1
-
-for x in plik:
-    line = x
-    line = line.strip()
-    line = line.split(" ")
-
-    if line[-1] in slownik:
-        slownik[line[-1]] = slownik.get(line[-1]) + [line]
-    else:
-        slownik[line[-1]] = [line]
+import math
+import re
+from KlasaDecyzyjna import KlasaDecyzyjna
 
 
-print("Istniejące klasy decycyzyjne + liczba elementów w klasie: ")
-for key in slownik:
-    print("{}, liczba elementów: {}".format(key, len(slownik[key])))
+class SystemDecyzyjny:
+    NUMBER_OF_ATTRIBUTES = 15
 
+    def printFile(self):
+        f = open(self)
+        print(f.read())
+
+    def getNumericAttributes(self):
+        i = 0
+        array = []
+        for elem in self:
+            if "n" in elem:
+                array.append(i)
+            i += 1
+        return array
+
+    def minAttributeValue(self, numericAttributes):
+        for i in numericAttributes:
+            array = []
+            for x in range(len(self)):
+                array.append(self[x][i])
+            print("attribute", i, "min value", min(array))
+
+    def maxAttributeValue(self, numericAttributes):
+        for i in numericAttributes:
+            array = []
+            for x in range(len(self)):
+                array.append(self[x][i])
+            print("attribute", i, "max value", max(array))
+
+    def separateClasses(self):
+        result = []
+        for i in self:
+            result.append(i[0:3])
+        return result
+
+    def sortAttrToSpecifiedClass(listOfClasses, listOfAttributes):
+        result = []
+        listIteration = 0
+        for i in listOfClasses:
+            x = KlasaDecyzyjna()
+            x.setKlasaDecyzyjna(i)
+            attributes = SystemDecyzyjny.switchColumnsToRows(listOfAttributes)
+            x.setAttributes(attributes[listIteration])
+            listIteration += 1
+            result.append(x)
+        return result
+
+    def switchColumnsToRows(self):
+        result = []
+        for x in range(len(self[0])):
+            row = []
+            for i in range(len(self)):
+                row.append(self[i][x])
+            result.append(row)
+        return result
+
+    def average(self):
+        sum = 0
+        for i in self:
+            sum = sum + float(i)
+        return sum / len(self)
+
+    def numberOfAttributesInClass(classes, numeric):
+        for i in range(len(classes)):
+            print(KlasaDecyzyjna.getKlasaDecyzyjna(classes[i]), "has", len(KlasaDecyzyjna.getAttributes(classes[i])),
+                  "attributes")
+
+    def standardDeviation(classes, numeric):
+        for i in numeric:
+            standDevList = KlasaDecyzyjna.getAttributes(classes[i])
+            standDevAvg = SystemDecyzyjny.average(standDevList)
+            sum = 0
+            for j in standDevList:
+                xi = float(j) - standDevAvg
+                xi2 = xi * xi
+                sum = sum + xi2
+            standDev = math.sqrt(sum / len(standDevList))
+            print(KlasaDecyzyjna.getKlasaDecyzyjna(classes[i]), "standard deviation:", standDev)
+
+    def splitIntoLines(self):
+        return re.split(r'\n', self)
+
+    def listAttributesAndTheirNumbers(self):
+        lines = SystemDecyzyjny.splitIntoLines(self)
+        myArray = []
+        for line in lines:
+            myArray.append(line.split(" "))
+        return myArray
+
+
+def main():
+    # SystemDecyzyjny.printFile("C01/australian-type.txt")
+    array = SystemDecyzyjny.splitIntoLines(open("C01/australian-type.txt").read())
+    print(array)
+    lines = SystemDecyzyjny.listAttributesAndTheirNumbers(open("C01/australian.txt").read())
+    SystemDecyzyjny.minAttributeValue(lines, SystemDecyzyjny.getNumericAttributes(array))
+    SystemDecyzyjny.maxAttributeValue(lines, SystemDecyzyjny.getNumericAttributes(array))
+    SystemDecyzyjny.switchColumnsToRows(SystemDecyzyjny.listAttributesAndTheirNumbers(open("C01/australian.txt").read()))
+    classes = SystemDecyzyjny.sortAttrToSpecifiedClass(SystemDecyzyjny.separateClasses(array),
+                                                       SystemDecyzyjny.listAttributesAndTheirNumbers(
+                                                           open("C01/australian.txt").read()))
+    SystemDecyzyjny.numberOfAttributesInClass(classes, SystemDecyzyjny.switchColumnsToRows(
+        SystemDecyzyjny.listAttributesAndTheirNumbers(open("C01/australian.txt").read())))
+    SystemDecyzyjny.standardDeviation(classes, SystemDecyzyjny.getNumericAttributes(array))
+
+
+if __name__ == "__main__":
+    main()
